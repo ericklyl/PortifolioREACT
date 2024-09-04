372cddf60d975aef35b43af7dee60c4bf34e62fd
Erick Loyola <109321308+ericklyl@users.noreply.github.com>
2024-09-04 01:42:25 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PortifolioREACT/',
   css: {
     modules: {
       localsConvention: "camelCase",
